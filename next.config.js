dbd17d103ebdea4ac161f5960bc3f524574397bf
andrzaha <andrej.zaharov@gmail.com>
2024-11-05 22:10:38 +0000

    update pachakges

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
+const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true
+  },
+  experimental: {
+    swcPlugins: [["@onlook/nextjs", {
+      root: path.resolve(".")
+    }]]
+  }
 };
-
 module.exports = nextConfig;
